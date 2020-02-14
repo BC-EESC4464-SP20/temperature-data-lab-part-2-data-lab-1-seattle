bf7dadcbefcf9776a0d3ab0486dd1c8ae227d21c
cannonkl <59878482+cannonkl@users.noreply.github.com>
2020-02-13 20:40:42 -0500

    blah

--- StationTempObs_Part2.m
+++ StationTempObs_Part2.m
@@ -56,6 +56,8 @@ title('Locations of stations with observational temperature data')
 %function cmocean, which is a good diverging colormap option
 %<--
 
+
+
 %% 4. Now calculate the projected future rate of temperature change at each of these 18 stations
 % using annual mean temperature data from GFDL model output following the
 % A2 scenario (here you will call the function StationModelProjections,
@@ -68,7 +70,7 @@ title('Locations of stations with observational temperature data')
 % below
 P_grid = NaN(length(sta),2);
 baseline_grid = NaN(length(sta),2);
-tempAnnMeanAnomaly_grid = NaN(94,18);
+tempAnnMeanAnomaly_grid = NaN(18,94);
 
 % Write a for loop that will use the function StationModelProjections to
 % extract from the model projections for each station:
@@ -83,13 +85,10 @@ for i=1:1:18
 P_grid(i,:)=P;
 baseline_grid(i,:)=baseline_model;
 
-tempAnnMeanAnomaly_grid(:,i)=tempAnnMeanAnomaly;
+tempAnnMeanAnomaly_grid(i,:)=tempAnnMeanAnomaly;
 end
     
-   
-
-
-
+  
 %% 5. Plot a global map of the rate of temperature change projected at each station over the 21st century
 %<--
 figure(3); clf
@@ -97,12 +96,12 @@ worldmap('World')
 load coastlines
 plotm(coastlat,coastlon)
 scatterm(lat,lon,120, P_grid(:,1).*10,'filled')
-mycolormap = customcolormap(linspace(0,1,11), {'#7f3c0a','#b35807','#e28212','#f9b967','#ffe0b2','#f7f7f5','#d7d9ee','#b3abd2','#8073a9','#562689','#2f004d'});
+mycolormap = customcolormap([0 .25 .5 .75 1], {'#9d0142','#f66e45','#ffffbb','#65c0ae','#5e4f9f'});
 colorbar('southoutside');
 colormap(mycolormap);
 axis off;
-%h=colorbar('southoutside');
-%h.Label.String= 'ºC per decade';'southoutside';
+h=colorbar('southoutside');
+h.Label.String= 'ºC per decade';'southoutside';
 title('Rate of projected temperature change from 2006 to 2099 (ºC per decade) ')
 
 
@@ -132,23 +131,19 @@ title('Baseline innterannual variabilty(standard deviation of annual mean temper
 %temperature trend will have reached 2x the standard deviation of the
 %temperatures from the baseline period
 
-year_grid = NaN(18,1);
+year_grid = NaN(1,18);
 
 for i= 1:1:18 
     
-    noise = 2*baseline_grid(i,2);
-    slope = P_grid(i,1);
-    intercept = P_grid(i,2);
+    signal = tempAnnMeanAnomaly_grid(i,:);
+    noise = baseline_grid(i,2)
+    I = find(signal/noise > 2,1)
     
-    Year_calc = (((noise)-intercept)/slope);
+    station_year=Year(I);
     
-    %I = find(tempAnnMeanAnomaly_grid(:,i) >= noise,1);
-    %station_year = Year(I);
+    year_grid(i)=station_year;
     
-    Year_round = round(Year_calc);
-    year_grid(i) = Year_round;
-    
-    
+      
 end 
 
 
@@ -158,7 +153,7 @@ figure(5); clf
 worldmap('World')
 load coastlines
 plotm(coastlat,coastlon)
-scatterm(lat,lon,120, year_grid,'filled')
+scatterm(lat,lon,120,year_grid,'filled')
 %colormap(autumn)
 h=colorbar;
-title('Baseline innterannual variabilty(standard deviation of annual mean temperature, 2006-2025')
+title('Year of emergence of temperature increase')
